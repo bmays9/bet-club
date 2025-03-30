2c25302a7235244d1d10ece49af792dbe2b025c1
Barry Mays <barrykmays@gmail.com>
2025-03-30 14:17:50 +0000

    jupyter notebook setup

--- score_predict/management/commands/update_fixtures.py
+++ score_predict/management/commands/update_fixtures.py
@@ -9,7 +9,7 @@ API_KEY = os.getenv("API_FOOTBALL_KEY")
 BASE_URL = "https://v3.football.api-sports.io/"
 
 HEADERS = {
-    "x-apisports-key": API_KEY
+    'x-rapidapi-key': API_KEY
 }
 
 LEAGUES = {
