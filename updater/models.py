ef2efc9663873ef7f254e013f4392d53166a0f23
Barry Mays <barrykmays@gmail.com>
2025-08-09 16:54:19 +0000

    SP: Updater model changes to separate results and fixtures

--- updater/models.py
+++ updater/models.py
@@ -3,13 +3,19 @@ from django.db import models
 from django.utils import timezone
 
 class UpdateTracker(models.Model):
-    last_fixtures_results_check = models.DateTimeField(null=True, blank=True)
+    last_fixtures_check = models.DateTimeField(null=True, blank=True)
+    last_results_check = models.DateTimeField(null=True, blank=True)
     last_tables_check = models.DateTimeField(null=True, blank=True)
 
-    def should_update_fixtures_results(self, interval_minutes=60):
-        if not self.last_fixtures_results_check:
+    def should_update_results(self, interval_minutes=60):
+        if not self.last_results_check:
             return True
-        return timezone.now() - self.last_fixtures_results_check >= timezone.timedelta(minutes=interval_minutes)
+        return timezone.now() - self.last_results_check >= timezone.timedelta(minutes=interval_minutes)
+
+    def should_update_fixtures(self, interval_days=10):
+        if not self.last_fixtures_check:
+            return True
+        return timezone.now() - self.last_fixtures_check >= timezone.timedelta(days=interval_days)
 
     def should_update_tables(self, interval_minutes=60):
         if not self.last_tables_check:
--- updater/utils.py
+++ updater/utils.py
@@ -8,16 +8,23 @@ def maybe_update():
     tracker, _ = UpdateTracker.objects.get_or_create(pk=1)
 
     UPDATE_INTERVAL_MINUTES = 60
+    UPDATE_INTERVAL_DAYS = 10
     RESULTS_DELAY_HOURS = 2
 
-    if tracker.should_update_fixtures_results(UPDATE_INTERVAL_MINUTES):
+    if tracker.should_update_results(UPDATE_INTERVAL_MINUTES):
         pending_fixtures = Fixture.objects.filter(
             date__lte=timezone.now() - timedelta(hours=RESULTS_DELAY_HOURS),
             status_code__lt=100
         )
         if pending_fixtures.exists():
-            call_command('update_fixtures', verbosity=0)
             call_command('update_results', verbosity=0)
             call_command('update_scores', verbosity=0)
-            tracker.last_fixtures_results_check = timezone.now()
+            tracker.last_results_check = timezone.now()
             tracker.save()
+
+    if tracker.should_update_fixtures(UPDATE_INTERVAL_DAYS):
+        call_command('update_fixtures', verbosity=0)
+        tracker.last_fixtures_check = timezone.now()
+        tracker.save()
+
+            
